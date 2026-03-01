771a856d48edefdeef6d9dd5369ec9e8346bff1a
ksenera <ksenera7130@conestogac.on.ca>
2026-02-28 20:39:30 -0500

    added Account class and constructor to create account number

--- AccountsClass.py
+++ AccountsClass.py
@@ -1,5 +1,11 @@
 class Customer: 
+
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+        self.first = first_name
+        self.last = last_name
+        
     
+class Account:
+
+    def __init__(self, balance, acc_number):
+        self.number = acc_number
