dd98527e295b2b66c81e7cb80a854c642ab38e97
ksenera <ksenera7130@conestogac.on.ca>
2026-02-28 20:42:18 -0500

    created blank methods for account

--- AccountsClass.py
+++ AccountsClass.py
@@ -9,3 +9,15 @@ class Account:
 
     def __init__(self, balance, acc_number):
         self.number = acc_number
+
+    def check_balance():
+        pass
+
+    def withdraw():
+        pass
+
+    def deposit():
+        pass
+
+    
+    
