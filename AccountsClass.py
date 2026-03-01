efcd150447634ff0c5e20b0d92dfafd74ad426e6
ksenera <ksenera7130@conestogac.on.ca>
2026-02-28 21:16:07 -0500

    got confused with parameter list and attribute internally generated

--- AccountsClass.py
+++ AccountsClass.py
@@ -5,16 +5,17 @@ class Customer:
     def __init__(self, first_name, last_name):
         self.first = first_name
         self.last = last_name
+        self.account = []
         
     
 class Account(ABC):
 
-    def __init__(self, balance, acc_number):
+    def __init__(self, acc_number):
         self.__number = acc_number
-        self.__balance = balance
+        self.__balance = 0
 
     def check_balance(self):
-        
+        pass
 
     def withdraw(self):
         pass
@@ -27,7 +28,8 @@ class Account(ABC):
         pass
 
 class SavingsAccount(Account):
-    def __init__(self, interest_rate):
+    def __init__(self, acc_number, interest_rate):
+        super().__init__(acc_number)
         self.__interest = interest_rate
 
     def calculate_return(self):
@@ -38,6 +40,8 @@ class SavingsAccount(Account):
 
 
 class CheckingAccount(Account):
+    def __init__(self, acc_number):
+        super().__init__(acc_number)
     def transaction(self):
         pass
 
